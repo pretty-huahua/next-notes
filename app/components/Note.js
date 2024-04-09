5374f09658a7980c74fcd7e2c21d16161a4b8ebe
ssy <1304795069@qq.com>
2024-04-09 22:44:15 +0800

    day3:笔记预览界面，loading.js使用股价屏，路由缓存

---
+++ app/components/Note.js
@@ -0,0 +1,21 @@
+import dayjs from 'dayjs';
+import EditButton from '@/app/components/EditButton'
+import NotePreview from '@/app/components/NotePreview'
+export default function Note({ noteId, note }) {
+  const { title, content, updateTime } = note
+
+  return (
+    <div className="note">
+      <div className="note-header">
+        <h1 className="note-title">{title}</h1>
+        <div className="note-menu" role="menubar">
+          <small className="note-updated-at" role="status">
+            Last updated on {dayjs(updateTime).format('YYYY-MM-DD hh:mm:ss')}
+          </small>
+            <EditButton noteId={noteId}>Edit</EditButton>
+        </div>
+      </div>
+      <NotePreview>{content}</NotePreview>
+    </div>
+  )
+}
